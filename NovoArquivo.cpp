27898bd2d5998daf6766057d3aa862ee709dae07
Dheryck-3d <87882125+Dheryck-3d@users.noreply.github.com>
2021-08-02 14:40:10 -0300

    Novo arqquivo

---
+++ NovoArquivo.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+#include <locale>
+int main(int argc, char** argv)
+{
+	char s;
+    int idade;
+    cout<< "Qual sua idade?";
+    cin>> idade;
+  if (idade>=18)
+cout<<"Você é maior de idade";
+else
+{
+	cout<<"Você é menor de idade";
+}
+cout <<"Qual seu sexo?";
+cin>> s;
+if(s=f)
+	cout<<"Você é do gênero feminino";
+else
+{
+	cout<<"Você é do gênero masculino";
+}
+	return 0;
+}
+#include <iostream>
+using namespace std;
+#include <locale>
+int main(int argc, char** argv)
+{
+	char s;
+    int idade;
+    cout<< "Qual sua idade?";
+    cin>> idade;
+    cout<<"Sua idade é:"<<idade;
+  if (idade>=18)
+cout<<"Você é maior de idade";
+else
+{
+	cout<<"Você é menor de idade";
+}	
+	return 0;
+}
+#include <iostream>
+using namespace std;
+#include <locale>
+int main(int argc, char** argv)
+{
+	char s;
+    int idade;
+    cout<< "Qual sua idade?";
+    cin>> idade;
+    cout<<"Sua idade é:"<<idade;
+	
+	return 0;
+}
+#include <iostream>
+using namespace std;
+#include <locale>
+int main(int argc, char** argv)
+{
+	char s;
+	int idade;
+	cout << "Qual seu sexo?";
+	cin>> s;
+	if 
+	cout<<"Seu sexo é :"<<s;
+	
+	return 0;
+}
+#include <iostream>
+using  namespace std;
+
+int main(int argc, char** argv)
+{
+	int n1; int n2; float so=0;
+	cout << "Digite o primeiro número";
+	cin>> n1;
+	cout << "Digite o segundo número";
+	cin>> n2;
+	so+= n1 + n2;
+	cout<< "O resultado da soma é de : " <<so;
+	return 0;
+}
