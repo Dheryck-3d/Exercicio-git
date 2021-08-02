ca365a76380f31d990d79de8379a5f55f79d0e05
Dheryck-3d <87882125+Dheryck-3d@users.noreply.github.com>
2021-08-02 14:39:48 -0300

    Teste arquivo

---
+++ Teste.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include <locale>
+int main(int argc, char** argv)
+{
+	char s;
+	int idade;
+	cout << "Qual seu sexo?";
+	cin>> s;
+	if 
+	cout<<"Seu sexo é :"<<s;
+	
+	return 0;
+}
